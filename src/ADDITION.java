d6f52eeb40f1f96815ae4933f28b9a29da07fae6
SuruG <surugholase@gmail.com>
2022-03-24 15:37:12 +0530

    first commit....

---
+++ src/ADDITION.java
@@ -0,0 +1,18 @@
+
+public class ADDITION {
+
+	int a,b,c;
+	
+	public void Addition(int a,int b) {
+		
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ADDITION addition=new ADDITION();
+		addition.Addition(2,30);
+	}
+
+}
